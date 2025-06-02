7261610b69545f2f32dcb3f1b547e5bddaae6d96
Sage <93442303+polrel-witter@users.noreply.github.com>
2025-06-02 16:10:15 +0000

    Switched DB connection to a Neon instance
    Supabase IPv6 connection wasn't working.

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+  connectionTimeoutMillis: 10000, // Return an error after 2 seconds if connection could not be established
 });
 
 // Handle pool errors
