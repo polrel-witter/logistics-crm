cf3aa1fb03f1725d71b3926362e5aeb529646402
Sage <93442303+polrel-witter@users.noreply.github.com>
2025-06-19 18:49:42 +0000

    Simplified to just serve our initial companies pg
    And:
    - Removed unncessary dir nesting; easier to run

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"logistics-crm/internal/handlers"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/companies", handlers.CompaniesHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
