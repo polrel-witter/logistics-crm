8bd111b58e7046201587d584578ecf7190830272
Sage <93442303+polrel-witter@users.noreply.github.com>
2025-06-25 20:36:14 +0000

    GetCompanyProfile connected to Apollo API

--- internal/integrations/apollo/client.go
+++ internal/integrations/apollo/client.go
@@ -1,6 +1,10 @@
 package apollo
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -15,7 +19,40 @@ type CompanyProfile struct {
 	Industry string `json:"industry"`
 }
 
+func (c *Client) buildApolloGETRequest(url string) (*http.Request, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Cache-Control", "no-cache")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("x-api-key", c.APIKey)
+
+	return req, nil
+}
+
 func (c *Client) GetCompanyProfile(domain string) (*CompanyProfile, error) {
-	// TODO: API call implementation
-	return &CompanyProfile{}, nil
+	url := fmt.Sprintf("https://api.apollo.io/api/v1/organizations/enrich?domain=%s", domain)
+
+	// Http request/response
+	req, _ := c.buildApolloGETRequest(url)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read body
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	fmt.Println(string(body))
+
+	// Parse body
+	var company CompanyProfile
+	err = json.Unmarshal(body, &company)
+	if err != nil {
+		// handle error
+		log.Printf("Error parsing JSON: %v", err)
+		return nil, err
+	}
+
+	return &company, nil
 }
